8065d4e2157c32d1064023f32e0e9c66c4f84095
Dhruv <ddrathod510@gmail.com>
2024-10-27 19:18:32 -0400

    adding methods

--- oops.java
+++ oops.java
@@ -3,9 +3,17 @@ class Pen {
     String type;
 
     public void write() {
+        System.out.println("Hello World!");
+    }
+
+    public void colourPrint() {
         System.out.println("The colour of the pen is: " + colour);
+    }
+
+    public void typePrint() {
         System.out.println("The type of the pen is: " + type);
     }
+
 }
 
 public class oops {
@@ -13,11 +21,17 @@ public class oops {
         Pen pen1 = new Pen();
         pen1.colour = "Blue";
         pen1.type = "Gel";
-        pen1.write();
 
         Pen pen2 = new Pen();
         pen2.colour = "Green";
         pen2.type = "Bold";
+
+        pen1.write();
+        pen1.typePrint();
+        pen1.colourPrint();
+
         pen2.write();
+        pen2.typePrint();
+        pen2.colourPrint();
     }
 }
