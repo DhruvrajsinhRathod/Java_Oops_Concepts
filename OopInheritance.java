f7170cfc6719608e7dc27c58783609fd84ef5f7e
Dhruv <ddrathod510@gmail.com>
2024-10-27 21:33:49 -0400

    inheritance added

---
+++ OopInheritance.java
@@ -0,0 +1,17 @@
+class Shape {
+    String colour;
+}
+
+class Square extends Shape {
+    public void squarePrinter() {
+        System.out.println(colour);
+    }
+}
+
+public class OopInheritance {
+    public static void main(String[] args) {
+        Square s1 = new Square();
+        s1.colour = "RedSquare";
+        s1.squarePrinter();
+    }
+}
